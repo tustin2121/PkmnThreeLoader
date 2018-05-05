de06940ecc22edb6caa37f20d313486b60a06b95
tustin2121 <tustin2121@gmail.com>
2018-05-05 15:49:42 -0400

    WebGL doesn't do Geometry Shaders
    ...something else will have to be done for them, when we get to that part...

--- spica/rendering/shaders/GeomShaderGenerator.js
+++ spica/rendering/shaders/GeomShaderGenerator.js
@@ -1,1 +1,6 @@
 // https://github.com/gdkchan/SPICA/blob/6ffdfdc1ddf3b3614b9fda457ef9717194d6cf34/SPICA.Rendering/Shaders/GeometryShaderGenerator.cs
+
+// WebGL does not support Geometry Shaders, so something different will have to be done.
+// see here: https://stackoverflow.com/questions/8641119/webgl-geometry-shader-equivalent
+// and here: https://acko.net/blog/yak-shading/
+
